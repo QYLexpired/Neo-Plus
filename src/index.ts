d13a7ed631eb743414e32be91548b7f283c87c99
QYLexpired <1525124972@qq.com>
2026-09-07 18:17:34 +0800

    1.6.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Plugin } from 'siyuan';
 import { NeoPlusController } from './main/controller';
-import { configKey } from './main/data';
 export default class NeoPlusPlugin extends Plugin {
   private controller: NeoPlusController | null = null;
   onload(): void {
@@ -11,7 +10,5 @@ export default class NeoPlusPlugin extends Plugin {
     this.controller?.destroy();
     this.controller = null;
   }
-  uninstall(): void {
-    this.removeData(configKey);
-  }
+  uninstall(): void {}
 }
